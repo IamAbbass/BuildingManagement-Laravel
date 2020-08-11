905dd4ace05e426ddd3f52102db7901be5507cf7
muhammadraees <muhammadraees486@gmail.com>
2020-08-11 15:11:38 +0500

    delete old files

--- Old Files/resources/views/layouts/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<div id="footer" class="page-footer hide">
-    <div class="d-flex p-3">
-        <span class="text-sm text-muted flex">&copy; Copyright. zeddevelopers.com</span>
-        <div class="text-sm text-muted">Version 1.0</div>
-    </div>
-</div>
