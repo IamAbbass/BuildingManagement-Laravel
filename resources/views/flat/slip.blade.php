fbe74f264c074b76c5ab06e853a88e66dfc0eda4
Zuhair Mustafa <zeddevelopers25@gmail.com>
2021-01-01 15:03:22 +0500

    Update slip.blade.php

--- resources/views/flat/slip.blade.php
+++ resources/views/flat/slip.blade.php
@@ -12,19 +12,19 @@
             padding: 5px;
             border-radius: 7px;
         }
-        .harmain_bg{
+        .container{
             background-image: url("{{ asset('img/logo.png') }}");
             background-color:#fff;
-            background-position: left top;
+            background-position: -35px -20px;
             background-repeat: no-repeat;
             background-size: 200px;
             border:1px solid #000;
-            margin:10px;
+            margin:40px 0;
         }
         .row{
            display: block;
-            padding:5px 0;
-            border-top:1px solid #999;
+           padding:0 0;
+           border-top:1px solid #999;
         }       
         .one_six{
             width:16.66%;
@@ -59,17 +59,21 @@
         }
         .amount{
         }
+        .seperator {
+            border-top: 3px dashed;
+            margin: 0 5px;
+        }
 
 	 </style>
 </head>
 <body>
     {{-- {{ $payment }} --}}
     
-	<div class="harmain_bg" >
-		<h1 class="text-center">Saima Square One Residents Tower Association<br /><small>Plot No. 1185/G, Stadium Road, Block 10-A, Gulshan-e-Iqbal, Karachi</small></h1>
-        <p></p>
-        
-        <h2 class="text-center"><kbd>Block-{{ $payment->flat->block->name }} / Flat# {{ $payment->flat->name }}</kbd></h2>
+	<!-- Office Copy -->
+    <div class="container" >
+        <h4 class="text-center"><kbd>Office Copy</kbd></h4>
+		<h4 class="text-center">Saima Square One Residents Tower Association<br /><small>Plot No. 1185/G, Stadium Road, Block 10-A, Gulshan-e-Iqbal, Karachi</small></h4>
+        <h4 class="text-center"><kbd>Block-{{ $payment->flat->block->name }} / Flat# {{ $payment->flat->name }}</kbd></h4>
         
         <div class="row">
             <div class="one_six bold"><span>Receipt No:</span></div>
@@ -98,25 +102,75 @@
         <div class="row">            
             <div class="one_two bold"><span>Amount:</span></div>
             <div class="one_two capitalize">
-                <span class="amount"><strong>{{ number_format($payment->payment) }} /-</strong></span>                
+                <span class="amount">{{ number_format($payment->payment) }} /-</span>                
             </div>
             <div class="clearfix"></div>
         </div>
        
         <div class="row" style="padding:5px">
-            <p>
-               <u style="text-align: left">This is an electronic generated receipt, no signature is required</u>
+            <p style="font-size: 11px">
+               <u style="text-align: left">This is an electronic generated receipt, no signature is required.</u>
                <span style="float: right;">
                <label>Printed By: </label>
-               <u >{{ auth()->user()->name }}</u>
+               {{ auth()->user()->name }}
                 at {{ date('d-M-Y h:i a') }}</span>
             </p> 
-
-
         </div>
-	
 	</div>
-	<script type="text/javascript">
+
+    <!-- Seperator -->
+    <div class="seperator"></div>
+
+	<!-- Resident Copy -->
+    <div class="container" >
+        <h4 class="text-center"><kbd>Resident Copy</kbd></h4>
+		<h4 class="text-center">Saima Square One Residents Tower Association<br /><small>Plot No. 1185/G, Stadium Road, Block 10-A, Gulshan-e-Iqbal, Karachi</small></h4>
+        <h4 class="text-center"><kbd>Block-{{ $payment->flat->block->name }} / Flat# {{ $payment->flat->name }}</kbd></h4>
+        
+        <div class="row">
+            <div class="one_six bold"><span>Receipt No:</span></div>
+            <div class="one_six"><span>{{ $payment->id }}</span></div>
+            
+            <div class="one_six text-right bold"><span>&nbsp;</span></div>
+            <div class="one_six"><span>&nbsp;</span></div>
+            
+            <div class="one_six text-right bold"><span>Date:</span></div>
+            <div class="one_six"><span>{{ $payment->date }}</span></div>            
+            <div class="clearfix"></div>
+        </div>    
+        
+        <div class="row">
+            <div class="one_two bold"><span>Received with thanks from Mr/Mrs.:</span></div>
+            <div class="one_two"><span>{{ $payment->flat->person_name }} </span></div>
+            <div class="clearfix"></div>
+        </div>
+        
+        <div class="row">            
+            <div class="one_two bold"><span>{{ $payment->account->name }} :</span></div>
+            <div class="one_two"><span>{{ $payment->month }}</span></div>
+            <div class="clearfix"></div>
+        </div>
+        
+        <div class="row">            
+            <div class="one_two bold"><span>Amount:</span></div>
+            <div class="one_two capitalize">
+                <span class="amount">{{ number_format($payment->payment) }} /-</span>                
+            </div>
+            <div class="clearfix"></div>
+        </div>
+       
+        <div class="row" style="padding:5px">
+            <p style="font-size: 11px">
+               <u style="text-align: left">This is an electronic generated receipt, no signature is required.</u>
+               <span style="float: right;">
+               <label>Printed By: </label>
+               {{ auth()->user()->name }}
+                at {{ date('d-M-Y h:i a') }}</span>
+            </p> 
+        </div>
+	</div>
+    
+    <script type="text/javascript">
 		window.print();
 	</script>
 </body>
